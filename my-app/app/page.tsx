3c961d004d63672a11f3b9c98e8c59983b5e4b21
UsmanKhursheed06 <usmankq04@gmail.com>
2025-10-24 05:40:55 +0500

    feat: add login page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,10 @@
+import { LandingPage } from "./(components)/LandingPageComponents";
+import Login from "./(components)/LoginPageComponents/Login";
+
 export default function Home() {
   return (
     <div>
-       
+       <Login />
     </div>
   );
 }
